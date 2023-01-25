7aed307712c8bc00592fad5431be38a9dda8cbb2
JayDeLux <iffrig@hotmail.com>
2023-01-25 22:04:26 +0100

    Added different command examples for Mac cs Linux.

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#tail -f -n60 /var/log/system.log 
-journalctl -f -n60 
+#tail -f -n60 /var/log/system.log   # On Debian
+journalctl -f -n60      # On Mac
+
+# ... or whatever command you like that generate STDOUT data.
